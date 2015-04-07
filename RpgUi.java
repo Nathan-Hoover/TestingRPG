4d360cccc08cded96eaa39df5033efc6ad3610f8
Nathaniel Hoover <nathaniel.l.hoover@gmail.com>
2015-04-07 18:20:51 -0500

    Creating Panel

--- RpgUi.java
+++ RpgUi.java
@@ -8,18 +8,42 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 class RpgUi {
+	JFrame frame;
+	JPanel optionPanel, mainPanel, statusPanel;
 	
 	RpgUi(){
 		createJFrame();
 	}
 	
 	private void createJFrame(){
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.black);
 		frame.setTitle("RpgUI");
 		frame.setLocationRelativeTo(null);
 		frame.setMinimumSize(new Dimension(1280 , 720));		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+	
+	private void createPanels(){
+		optionPanel = new JPanel();
+		optionPanel.setOpaque(false);
+		optionPanel.setBackground(Color.red);
+		mainPanel = new JPanel();
+		mainPanel.setOpaque(false);
+		statusPanel = new JPanel();
+		statusPanel.setOpaque(false);
+		
+		GridBagConstraints panelConstraints = new GridBagConstraints();
+		
+		panelConstraints.weightx = 1;
+		panelConstraints.weighty = 1;
+		panelConstraints.gridx = 0;
+		panelConstraints.gridy = 0;
+		panelConstraints.anchor = GridBagConstraints.CENTER;
+		
+		frame.add(optionPanel, panelConstraints);
+		frame.add(mainPanel, panelConstraints);
+		frame.add(statusPanel, panelConstraints);
 	}
 	
 }
