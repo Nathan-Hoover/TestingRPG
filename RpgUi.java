e51f64f4918b0214179404ebc62e5d2b6fb2a6f3
Nathaniel Hoover <nathaniel.l.hoover@gmail.com>
2015-04-07 17:25:47 -0500

    Start of RPG Interface
    Start of many Headaches

---
+++ RpgUi.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+class RpgUi {
+	
+}
