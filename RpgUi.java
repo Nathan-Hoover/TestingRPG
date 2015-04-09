23fbc02471337c7e65d6457c4a4a845c889addeb
Brandon Bauman <brandon.bauman@hotmail.com>
2015-04-09 17:09:54 -0500

    panel something

--- RpgUi.java
+++ RpgUi.java
@@ -9,41 +9,92 @@ import javax.swing.event.DocumentListener;
 
 class RpgUi {
 	JFrame frame;
-	JPanel optionPanel, mainPanel, statusPanel;
+	JPanel topPanel, bottomPanel, sidePanel1, sidePanel2;
+	JTextArea textPanel;
 	
 	RpgUi(){
 		createJFrame();
+		createPanels();
+		createTextArea();
 	}
 	
 	private void createJFrame(){
 		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.black);
 		frame.setTitle("RpgUI");
+		frame.setLayout(new GridBagLayout());
 		frame.setLocationRelativeTo(null);
-		frame.setMinimumSize(new Dimension(1280 , 720));		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setMinimumSize(new Dimension(1280 , 720));
+		frame.setMaximumSize(new Dimension(1280 , 720));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 	
 	private void createPanels(){
-		optionPanel = new JPanel();
-		optionPanel.setOpaque(false);
-		optionPanel.setBackground(Color.red);
-		mainPanel = new JPanel();
-		mainPanel.setOpaque(false);
-		statusPanel = new JPanel();
-		statusPanel.setOpaque(false);
+		topPanel = new JPanel();
+		topPanel.setBackground(Color.green);
 		
-		GridBagConstraints panelConstraints = new GridBagConstraints();
+		bottomPanel = new JPanel();
+		bottomPanel.setBackground(Color.yellow);
 		
-		panelConstraints.weightx = 1;
-		panelConstraints.weighty = 1;
-		panelConstraints.gridx = 0;
-		panelConstraints.gridy = 0;
-		panelConstraints.anchor = GridBagConstraints.CENTER;
+		sidePanel1 = new JPanel();
+		sidePanel1.setBackground(Color.red);
 		
-		frame.add(optionPanel, panelConstraints);
-		frame.add(mainPanel, panelConstraints);
-		frame.add(statusPanel, panelConstraints);
+		sidePanel2 = new JPanel();
+		sidePanel2.setBackground(Color.red);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.PAGE_START;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		
+		frame.add(topPanel, gbc);
+		
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.gridy = 1;
+		gbc.weighty = 0.65;
+		
+		frame.add(bottomPanel, gbc);
+		
+		gbc.anchor = GridBagConstraints.PAGE_END;
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.weightx = 0.3;
+		
+		frame.add(sidePanel1, gbc);
+		
+		gbc.anchor = GridBagConstraints.PAGE_END;
+		gbc.gridy = 1;
+		frame.add(sidePanel2, gbc);
+		/*
+		GridBagConstraints gbc2 = new GridBagConstraints();
+		gbc2.anchor = GridBagConstraints.PAGE_END;
+		gbc2.fill = GridBagConstraints.BOTH;
+		gbc2.gridx = 0;
+		gbc2.gridy = 0;
+		gbc2.weightx = 1.0;
+		gbc2.weighty = 1.0;
+		
+		frame.add(sidePanel, gbc2);
+		*/
+		frame.pack();
+	}
+	
+	private void createTextArea(){
+		textPanel = new JTextArea(600, 800);
+		textPanel.insert("Test", 4);
+		
+		GridBagConstraints gbc3 = new GridBagConstraints();
+		gbc3.anchor = GridBagConstraints.PAGE_END;
+		gbc3.fill = GridBagConstraints.BOTH;
+		gbc3.gridx = 0;
+		gbc3.gridy = 1;
+		gbc3.weightx = 1.0;
+		gbc3.weighty = 1.0;
+		
+		frame.add(textPanel, gbc3);
 	}
 	
 }
